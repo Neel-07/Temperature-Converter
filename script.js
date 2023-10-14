62b5622f2a19b62dc97b57017dc1e36fda6222b5
Sharjeel-Afridi <afridisharjeel8@gmail.com>
2023-10-14 23:11:35 +0530

    improved the code and its working now

--- script.js
+++ script.js
@@ -7,90 +7,48 @@ function roundNumber(number) {
   return Math.round(number * 100) / 100;
 }
 
-function convertTemperatures() {
-
-  let cTemp = parseFloat(celsiusInput.value);
-  let fTemp = (cTemp * 9) / 5 + 32;
-  let kTemp = cTemp + 273.15;
-
-  fahrenheitInput.value = roundNumber(fTemp);
-  kelvinInput.value = roundNumber(kTemp);
-}
-
-celsiusInput.addEventListener("input", convertTemperatures);
-fahrenheitInput.addEventListener("input", function () {
-  let fTemp = parseFloat(fahrenheitInput.value);
-  let cTemp = ((fTemp - 32) * 5) / 9;
-  let kTemp = ((fTemp - 32) * 5) / 9 + 273.15;
-
-  celsiusInput.value = roundNumber(cTemp);
-  kelvinInput.value = roundNumber(kTemp);
-});
-kelvinInput.addEventListener("input", function () {
-  let kTemp = parseFloat(kelvinInput.value);
-  let cTemp = kTemp - 273.15;
-  let fTemp = ((kTemp - 273.15) * 9) / 5 + 32;
-
-  celsiusInput.value = roundNumber(cTemp);
-  fahrenheitInput.value = roundNumber(fTemp);
-
+function fromCelsius() {
     let cTemp = parseFloat(celsiusInput.value);
-    if (!isNaN(cTemp)) {
-    let fTemp = (cTemp * 9/5) + 32;
-    let kTemp = cTemp + 273.15;
 
-    fahrenheitInput.value = roundNumber(fTemp);
-    kelvinInput.value = roundNumber(kTemp);
+    if (!isNaN(cTemp)){
+        let fTemp = (cTemp * 9) / 5 + 32;
+        let kTemp = cTemp + 273.15;
+
+        fahrenheitInput.value = roundNumber(fTemp);
+        kelvinInput.value = roundNumber(kTemp);
     }
-    else 
-    {
-        if(celsiusInput.value==="")
-            alert("Please enter a valid numerical value");
-        celsiusInput.value = "";
-        fahrenheitInput.value="";
-        kelvinInput.value="";
+  
+}
+
+
+function fromFahrenheit() {
+    let fTemp = parseFloat(fahrenheitInput.value);
+
+    if (!isNaN(fTemp)){
+        let cTemp = ((fTemp - 32) * 5) / 9;
+        let kTemp = ((fTemp - 32) * 5) / 9 + 273.15;
+
+        celsiusInput.value = roundNumber(cTemp);
+        kelvinInput.value = roundNumber(kTemp);
     }
 }
 
-celsiusInput.addEventListener('input', convertTemperatures);
-fahrenheitInput.addEventListener('input', function() {
-    let fTemp = parseFloat(fahrenheitInput.value);
-    if (!isNaN(fTemp)) {
-    let cTemp = (fTemp - 32) * 5/9;
-    let kTemp = (fTemp - 32) * 5/9 + 273.15;
-
-    celsiusInput.value = roundNumber(cTemp);
-    kelvinInput.value = roundNumber(kTemp);
-    }
-    else 
-    {
-        if(fahrenheitInput.value!=="")
-            alert("Please enter a valid numerical value");
-        celsiusInput.value = "";
-        fahrenheitInput.value="";
-        kelvinInput.value="";
-    }
-});
-kelvinInput.addEventListener('input', function() {
+function fromKelvin() {
     let kTemp = parseFloat(kelvinInput.value);
-    if (!isNaN(kTemp)) {
 
-    let cTemp = kTemp - 273.15;
-    let fTemp = (kTemp - 273.15) * 9/5 + 32;
+    if(!isNaN(kTemp)){
+        let cTemp = kTemp - 273.15;
+        let fTemp = ((kTemp - 273.15) * 9) / 5 + 32;
 
-    celsiusInput.value = roundNumber(cTemp);
-    fahrenheitInput.value = roundNumber(fTemp);
+        celsiusInput.value = roundNumber(cTemp);
+        fahrenheitInput.value = roundNumber(fTemp);
     }
-    else 
-    {
-        if(fahrenheitInput.value!=="")
-            alert("Please enter a valid numerical value");
-        celsiusInput.value = "";
-        fahrenheitInput.value="";
-        kelvinInput.value="";
-    }
+    
+}
 
-});
+celsiusInput.addEventListener('input', fromCelsius);
+fahrenheitInput.addEventListener('input', fromFahrenheit);
+kelvinInput.addEventListener('input', fromKelvin)
 
 clearButton.addEventListener("click", function () {
   celsiusInput.value = "";
